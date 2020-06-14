94ba547b394e2fbb5ede6e89a567a00c73fe1264
Arch <33100644+mttorres@users.noreply.github.com>
2020-06-14 06:09:18 +0000

    Adicionando novas constantes e Enums

--- headers/constants.h
+++ headers/constants.h
@@ -1,4 +1,19 @@
+#ifndef CTE_H
+
 #define CTE_H
 
+const char* mappingEnumTable[] =  {
+    "GLOBAL",
+    "FUNC",
+    "LOOP",
+    "IF_BLOC",
+    "ELSE_BLOCK",
+};
 
-const char* mappingEnumTable[];
+const char* mappingEnumEntry[] =  {
+    "VAR",
+    "TDS",
+    "T_DIRECTIVE",
+};
+
+#endif
