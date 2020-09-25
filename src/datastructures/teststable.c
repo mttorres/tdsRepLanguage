d827ac3ff1d471562242c9af183fa60258c77a27
Arch <33100644+mttorres@users.noreply.github.com>
2020-09-24 22:52:40 -0400

    Testando pós processamento básico (atribuição) #9

--- src/datastructures/teststable.c
+++ src/datastructures/teststable.c
@@ -1,25 +1,161 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-//#include "../../headers/Node.h"
+#include "../../headers/Node.h"
 #include "../../headers/STable.h"
 
+
+
+
 /*
-Object* process(Node* n,  STable* currentScope) 
+	Processa cada nó em "pós ordem transversal"
+	
+	indo sempre para o nó mais abaixo e depois resolvendo as dependencias de todos os filhos desse nó antes de voltar para o originador da chamada
+	isso garante precedência de operações.
+
+	E a operação pós ordem é: AVALIAR RESULTADO ou ESCREVER NO NUXMV
+
+*/
+
+void* processData(Node* node, STable* currentScope) 
+{
+	int* sint = (int*) malloc(sizeof(int));
+	sint[0] = atoi(node->leafs[0]);
+	printf("[DEBUG-processData] SINTH: %d \n",sint[0]);
+	void* ip[] = {&sint[0]};
+
+	// vai precisar alocar esse inteiro...
+	Object* o = createObject(NUMBER_ENTRY, 1 , ip); 
+	
+	printObject(o);
+
+	return o;
+}
+
+
+void* process(Node* n,  STable* currentScope) 
 {
 
 	int info = n != NULL;	
-	if(info) {		
-		int i = 0;
+	if(info) {
+		void** SYNTH_O[n->nchild];	// note-se que as folhas não necessitam ser sintetizadas (porém! Devem ser passadas para o nó de cima em alguns casos, função DEVE SER VOID*)
+		int i = 0;					// no final das contas o object so serve para facilitar a análise de tipos (já que a linguagem não declara tipos)
 		if(n->children){
-			for(i= 0; i < n->nchild; i++){
-				if(n->children[i]) {
-					process(n->children[i], currentScope); // se tiver filho (desce um nível e resolve as dependencias) 
-				}
-
-
+			for(i= 0; i < n->nchild; i++)
+			{
+				if(n->children[i]) 
+				{
+					//printf("(%d)    ",i);
+					SYNTH_O[i] =  process(n->children[i], currentScope); // se tiver filho (desce um nível e resolve as dependencias) 
+				}														 // resolver dependencias realmente necessário? Parando para pensar podemos acessar o filho imediatamente abaixo do nó em questão e já pegar os valores ! Evita criar mais structs! (pode ficar complexo para alguns casos por outro lado... e fora que inviabiliza tds e vetores)
 			}
 		}
+
+		// por enquanto vamos testar bem simples...
+
+		// sintetiza token de dado 
+		if(n->type == DATA_NUM)
+		{
+			return processData(n,currentScope);
+		}
+
+
+
+		// note que os filhos são  (nome da variável),  (objeto)  (isso me faz perguntar... usar Object é uma boa?)
+
+			// não usar object -> "subir" com o valor já convertido (não da para "saber o tipo", não pode fazer typeof(valor) e o nó de referência vai estar perdido)
+			// deixa a verificação de tipos bem mais complexa...
+
+			// usar object ->  sobe sempre um objeto,  em alguns casos, tipo o nó ASSIGN_V pode só ter folha, por outro lado se esse nós "extra" não existe não conseguimos saber 
+			// se vamos acessar atributos de uma variavel ou uma variavel simples !
+			
+
+
+		if(n->type == ASSIGN_V)
+		{
+			printf("[DEBUG-ASSIGN_V] SINTH: %s \n",n->leafs[0]);
+			return n->leafs[0];
+		}
+
+		// salva valor na varíavel (Tabela)
+		// verifica-se pela operação (=)
+		if(n->type == OTHER_ASSIGN)
+		{	
+
+				// é um caso muito importante!
+				// DEVE-SE, verificar C_TIME,  se for 0, (processa como init), 
+				//caso contrário cria condição de inicalização ou next 
+				// init ou next ? deve olhar a tabela antes (se está lá, next, caso contrário init)
+
+				// nota importante (em métodos, C_TIME, I_TIME, F_TIME sempre será disponibilizado "indiretamente para as funções")
+				// mesma coisa para escopos (escopo podemos "subir" várias vezes, ou ... gastar memória salvando eles nos escopos filhos, (mas seria semanticamente incorreto))
+				// para métodos seria mais valido porque temos o parâmetro methodParam
+
+				TableEntry* currenttime = lookup(currentScope,"C_TIME");
+
+				int ctime = *(int*) currenttime->val->values[currenttime->val->OBJECT_SIZE-1];
+				if(ctime == 0)
+				{			
+
+					// caso de init, mudança vai se vai ter que mudar na tabela do smv 
+								
+
+					if(lookup(currentScope, (char*) SYNTH_O[0])) 
+					{
+						printf("[DEBUG-OTHER_ASSIGN] found it \n");
+
+						int method = 0; // criar lógica mais sofisticada para isso, por enquanto apenas passar por variavel
+
+						addValueCurrentScope((char *) SYNTH_O[0], (Object*) SYNTH_O[1],method,currentScope);	
+
+						printf("[DEBUG-OTHER_ASSIGN] caso init (sobrescrita de valor, deve mudar smv) \n");
+
+
+					}
+					else
+					{
+						printf("[DEBUG-OTHER_ASSIGN] caso init , deve CRIAR SMV \n");
+						
+						int method = 0; 
+
+						printf("[DEBUG-OTHER_ASSIGN] VALOR FOI SINTETIZADO? (nchild: %d) \n",n->nchild);
+
+						printObject((Object*) SYNTH_O[1]);
+
+						addValueCurrentScope((char *) SYNTH_O[0], (Object*) SYNTH_O[1],method,currentScope);	
+					}
+				}
+				else 
+				{
+					int method = 0; // criar lógica mais sofisticada para isso, por enquanto apenas passar por variavel
+					
+					addValueCurrentScope((char*) SYNTH_O[0], (Object *) SYNTH_O[1],method,currentScope); // sobrescrita (init)					
+
+					if(lookup(currentScope, (char*) SYNTH_O[0])) 
+					{
+						printf("[DEBUG-OTHER_ASSIGN] found it \n");
+
+
+						printf("[DEBUG-OTHER_ASSIGN] caso NEXT (next com condição) \n");
+
+
+					}
+					else
+					{
+						printf("[DEBUG-OTHER_ASSIGN] caso init (na verdade um next, onde o init foi null ?) com condição \n");
+					}					
+				}	
+
+				return NULL;	
+
+				// repare que tudo isso aqui vai ser extraido para um método (evita redundancia)
+		}
+
+	}
+}
+
+
+/*		
 		// ao chegar nas folhas avaliar a informação delas (todas "conjunto") e recuperar dependencias resolvidas do nível mais abaixo 
 		if(n->leafs) {
 			// for necessário ?
@@ -42,9 +178,10 @@ Object* process(Node* n,  STable* currentScope)
 
 			//}
 		}
-	}	
-}
 */
+	
+
+
 
 
 int main()
@@ -82,6 +219,7 @@ int main()
 	TableEntry* F_TIME = createEntry("F_TIME",num3,0,global);
 		
 
+/*
 	int valboo = 0;
 
 	void* pbool[] = {&valboo};
@@ -122,8 +260,10 @@ int main()
 	// else associado (ordem 2("1"), nível 1)
 	//addSubScope(global,ELSE_BLOCK);
 	printTable(addSubScope(global,ELSE_BLOCK));
-    
-/*
+ 
+*/    
+
+
 
     // testando mudanças de Node e fluxo de construção, programa simples com atribuição de variável
 
@@ -141,6 +281,12 @@ int main()
 			// Solução adotada... criar "struct" do objeto da linguagem? (apesar que a E_TABLE faz a mesma coisa não?)
 			// por outro lado: https://stackoverflow.com/questions/29381067/does-c-and-c-guarantee-the-ascii-of-a-f-and-a-f-characters
 
+
+	insert(global, I_TIME);
+	insert(global, C_TIME);
+	insert(global, F_TIME);
+
+
 	Node* expr = createNode(5,0,1,"RAWNUMBERDATA",DATA_NUM,"10");
 
 	Node* assignable = createNode(5,0,1,"ASSIGNABLE",ASSIGN_V,"v");	
@@ -153,11 +299,18 @@ int main()
 
 	
 	//printNode(root);
+
+
+	printf(" TABELA INICIADA : \n");
+
+
+	printTable(global);
 	
 
-	process(root, global);
+	printf(" ----------------------------------- \n");
 
-*/
+
+	process(root, global);
 
 	printTable(global);
 	
