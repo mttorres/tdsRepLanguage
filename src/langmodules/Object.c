6592ba751535cfe8e8452a18456c7b02291133cc
Arch <33100644+mttorres@users.noreply.github.com>
2020-10-06 18:46:28 -0400

    Alocando inteiro para referência de indices (#23)

--- src/langmodules/Object.c
+++ src/langmodules/Object.c
@@ -14,6 +14,25 @@ const char* mappingEnumObjectType[] =  {
 };
 
 
+void* allocatePtObjects(int type, void* value) 
+{
+	if(type == NUMBER_ENTRY || type == T_DIRECTIVE_ENTRY || type == SMV_POINTER)
+	{
+		int* pt = malloc(sizeof(int));
+		*pt = *(int*) value;
+		return pt;
+	}
+	
+	// precisa alocar para strings ? (pode ser que já tenha sido alocado pelo parser)
+	if(type == LABEL_ENTRY) 
+	{
+		//TODO (ao fazer o pós processamento)
+	}
+
+
+}	
+
+
 Object* createObject(int type, int OBJECT_SIZE, void** values) 
 {
 
@@ -28,7 +47,12 @@ Object* createObject(int type, int OBJECT_SIZE, void** values)
 		int i;
 		for(i = 0; i< OBJECT_SIZE; i++)
 		{
-			vo[i] = values[i];
+			//vo[i] = values[i];
+
+			// caso do int:  ele chega aqui como um ponteiro para um variavel local (essa região de memoria é perdida depois!)
+			// por ser um ponteiro para void que estamos guardando temos duas opções, alocar o ponteiro associado e passar para vo[i]
+			// ou alocar antes e passar para esse já alocado! (acho melhor a primeira opção, para centralizar)
+			vo[i] = allocatePtObjects(type,values[i]);			
 			printf("[createObject] valor: %d \n",*(int*) values[i]);
 		}
 		newOb->values = vo;	
