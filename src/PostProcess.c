f32b8dd754fc0a1886d8789d70666bd99f79bb14
Arch <33100644+mttorres@users.noreply.github.com>
2020-10-22 23:23:29 -0300

    Estrutura básica do PÓS PROCESSAMENTO #9

---
+++ src/PostProcess.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "../headers/PostProcess.h"
+
+
+
+void* eval(Node* n, STable* scope, STable** writeSmvTypeTable, HeaderController* controllerSmv)
+{
+	printf("[PostProcess - eval] test compile \n");
+}
+
