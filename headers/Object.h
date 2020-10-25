183e64e87e22a9eef4f1c3af7f3bad79a9bb4dae
Arch <33100644+mttorres@users.noreply.github.com>
2020-10-25 00:41:31 -0300

    Primeira versão do mapeamento (#31)

--- headers/Object.h
+++ headers/Object.h
@@ -10,7 +10,7 @@ typedef struct SYNTH_OBJECT
 {
   int type;
   int OBJECT_SIZE;
-  int STR;
+  int STR; // tamanho strings mapeado
   void ** values; // a pergunta é ... lista de valores ou valor único ?(em geral vai ser unico, pode ser também um ponteiro para uma lista)
   int changedType; // serve para auxiliar na escrita no nuXmv (e talvez em um futuro se permitir lista de valores diferentes)
   int binded;
