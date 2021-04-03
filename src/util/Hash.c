b91f5402c7befc5f2523f6116c21c2b4ae62a369
Arch <33100644+mttorres@users.noreply.github.com>
2021-04-03 02:26:56 -0300

    Correção da função hash

--- src/util/Hash.c
+++ src/util/Hash.c
@@ -2,14 +2,39 @@
 // Created by mateus on 27/03/2021.
 //
 
+
+#include <stdio.h>
+#include <stdlib.h>
 #include "Hash.h"
 
+/*
 int hash(char* str, int SIZE_ED){
+    int LARGE_PRIME = 2049;
     int hash = 401;
     while (*str != '\0') {
         hash = ((hash << 4) + (int)(*str)) % SIZE_ED;
         str++;
     }
-    return hash % SIZE_ED;
+    return hash % LARGE_PRIME;
 }
+*/
+
+int hash(char *str, int SIZE_ED)
+{
+    int hash = 5381;
+    int c;
+    char* strpt = str;
+
+    while (c = *strpt++){
+        hash = (( ( (hash << 5) + hash) + c )  % SIZE_ED); /* hash * 33 + c */
+    }
+    int final = hash % SIZE_ED;
+    if(final >= SIZE_ED-1){
+        fprintf(stderr,"[hash] Error in generating HASH\n");
+        exit(-1);
+    }
+    return final;
+}
+
+
 
