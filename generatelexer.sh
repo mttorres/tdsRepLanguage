602430ff5434e5b849555fed0d09fc6d913c620d
Arch <33100644+mttorres@users.noreply.github.com>
2021-04-21 01:44:59 -0300

    Limpeza de diretorio

--- generatelexer.sh
+++
@@ -1,3 +0,0 @@
-flex tdsrpl.l 
-gcc lex.yy.c -o tdsLEXER
-./tdsLEXER ../sample/merger.tdsrpl > LEXEROUTPUT
--- generateparsing.sh
+++
@@ -1,2 +0,0 @@
-bison  -d src/flex-bison/tdsrpl.y -b src/flex-bison/tdsrpl
-flex -o src/flex-bison/lex.yy.c src/flex-bison/tdsrpl.l
